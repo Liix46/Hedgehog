1f0e51750d43a62a4ee87875e3a1a6d10a96c9b7
alxb46 <alexboichev46@gmail.com>
2023-05-22 14:49:42 +0300

    add project

---
+++ Hedgehog/Hedgehog.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hedgehog
+{
+	public class Hedgehog
+	{
+        public static int Solve(int[] population, int desiredColor)
+        {
+            if (desiredColor < 0 || desiredColor >= 3)
+            {
+                throw new ArgumentOutOfRangeException(nameof(desiredColor));
+            }
+
+            if (population.Sum() == 0)
+            {
+                return 0;
+            }
+
+
+            int encounters = 0;
+
+            var minColor = Int32.MaxValue;
+            var maxColor = -1;
+
+            var minColorIndex = -1;
+            var maxColorIndex = -1;
+
+            for (int i = 0; i < population.Length; i++)
+            {
+                if (i != desiredColor)
+                {
+
+                    if (population[i] > maxColor && i != minColorIndex)
+                    {
+                        maxColor = population[i];
+                        maxColorIndex = i;
+                    }
+
+                    if (population[i] < minColor)
+                    {
+                        minColor = population[i];
+                        minColorIndex = i;
+                    }
+
+                  
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"{population[0]}, {population[1]}, {population[2]}");
+                if (population[minColorIndex] == population[maxColorIndex])
+                {
+                    if (population[minColorIndex] == 0)
+                    {
+                        return encounters;
+                    }
+
+                    population[minColorIndex]--;
+                    population[maxColorIndex]--;
+                    population[desiredColor] += 2;
+                }
+                else if (
+                        population[minColorIndex] - population[maxColorIndex] == -1
+                     || population[minColorIndex] - population[maxColorIndex] == 1)
+                {
+                    return -1;
+                }
+                else
+                {
+                    population[minColorIndex] += 2;
+                    population[maxColorIndex]--;
+                    population[desiredColor] --;
+                }
+
+                encounters++;
+            }
+
+        }
+
+    }
+}
---
+++ Hedgehog/Program.cs
@@ -0,0 +1,28 @@
+﻿using Hedgehog;
+
+
+//Console.WriteLine(Hedgehog.Hedgehog.Solve(new[] { 3, 6, 9 }, 0)); // 9
+
+//Console.WriteLine(Hedgehog.Hedgehog.Solve(new[] { 8, 1, 9 }, 0)); // -1
+
+//Console.WriteLine(Hedgehog.Hedgehog.Solve(new[] { 1, 1, 1 }, 2)); // -1
+
+//int result = Hedgehog.Hedgehog.Solve(new[] { 3, 6, 9 }, 0);
+//if (result == -1)
+//{
+//    Console.WriteLine("hedgehogs will not be able to reach their goal");
+//}
+//else
+//{
+//    Console.WriteLine($"hedgehogs will be able to reach their goal in so many encounters: {result}");
+//}
+
+int result = Hedgehog.Hedgehog.Solve(new[] { 0, 20, 9 }, 2);
+if (result == -1)
+{
+    Console.WriteLine("hedgehogs will not be able to reach their goal");
+}
+else
+{
+    Console.WriteLine($"hedgehogs will be able to reach their goal in so many encounters: {result}");
+}
